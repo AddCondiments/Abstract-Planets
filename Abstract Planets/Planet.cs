17ecafb70cc3ff31ebf821aa2ffee91cbc1684d8
Matthew Watada <mwatada@my.centennialcollege.ca>
2017-07-28 19:18:13 -0400

    Done the "Planet" class ...Ithink. Private instance variable, public properties, constructer and the ToString() method.

--- Abstract Planets/Planet.cs
+++ Abstract Planets/Planet.cs
@@ -22,20 +22,114 @@ namespace Abstract_Planets
     {
         // Private Instance Variables
 
-       
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
 
         // Public Properties
 
+        public double Diameter
+        {
+            get
+            {
+                return this._diameter;
+            }
+        }
 
+        public double Mass
+        {
+            get
+            {
+                return this._mass;
+            }
+        }
+
+        public int MoonCount
+        {
+            get
+            {
+                return this._moonCount;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+        }
+
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return this._orbitalPeriod;
+            }
+            set
+            {
+                this._orbitalPeriod = value;
+            }
+        }
+
+        public int RingCount
+        {
+            get
+            {
+                return this._ringCount;
+            }
+            set
+            {
+                this._ringCount = value;
+            }
+        }
+
+        public double ROtationPeriod
+        {
+            get
+            {
+                return this._rotationPeriod;
+            }
+            set
+            {
+                this._rotationPeriod = value;
+            }
+        }
+
+        /// <summary>
+        /// This constructer takes 3 parameters, and instanciates them to the private instance variables (_name, _diameter, and _mass).
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="diameter"></param>
+        /// <param name="mass"></param>
 
         // Constructors
 
+        public Planet(string name, double diameter, double mass)
+        {
+            this._name = name;
+            this._diameter = diameter;
+            this._mass = mass;
+        }
 
-
-        // Private Methods
+        // Private Methods (None that I can see)
 
 
 
         // Public Methods
+
+        /// <summary>
+        /// The ToString() method fromats the output in the console for objects of type 'Planet', or those that inherit from it.
+        /// </summary>
+        /// <returns></returns>
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}\nDiameter: {1}\nMass: {2}\n", Name, ,Diameter, Mass);
+        }
     }
 }
