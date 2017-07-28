7958379fe790d42803659c8d65b18e3c0d6e8835
Matthew Watada <mwatada@my.centennialcollege.ca>
2017-07-28 18:55:46 -0400

    Starting just with building the abstract class "Planet".

---
+++ Abstract Planets/Planet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* July 28th
+ * Matthew Watada
+ * 300832220
+ * Starting late I know, but doable.
+ * Version 0.1: starting with the abstract class "Planet". 
+ * Versions will update based on items added to the 
+ * project, and not based on iterations of the same file.
+ */
+
+namespace Abstract_Planets
+{
+    /// <summary>
+    /// This is the Planet class!
+    /// </summary>
+    abstract class Planet
+    {
+        // Private Instance Variables
+
+       
+
+        // Public Properties
+
+
+
+        // Constructors
+
+
+
+        // Private Methods
+
+
+
+        // Public Methods
+    }
+}
